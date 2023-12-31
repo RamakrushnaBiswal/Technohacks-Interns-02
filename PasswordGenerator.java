76c32625ffd5d492ed9a2d631712e2aea9251e9a
Ramakrushna Biswal <ramakrushnabunty@gmail.com>
2023-12-31 22:25:29 +0530

    added

---
+++ PasswordGenerator.java
@@ -0,0 +1,51 @@
+import java.security.*;
+
+public class PasswordGenerator {
+
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*()-_=+[]{}|;:'\",.<>/?";
+
+    public static String generatePassword(int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Password length must be greater than 0");
+        }
+
+        StringBuilder password = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+
+        // Add at least one character from each category
+        password.append(getRandomChar(UPPER, random));
+        password.append(getRandomChar(LOWER, random));
+        password.append(getRandomChar(DIGITS, random));
+        password.append(getRandomChar(SPECIAL, random));
+
+        // Fill the remaining length with random characters
+        for (int i = 4; i < length; i++) {
+            String allChars = UPPER + LOWER + DIGITS + SPECIAL;
+            password.append(getRandomChar(allChars, random));
+        }
+
+        // Shuffle the characters in the password
+        for (int i = password.length() - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            char temp = password.charAt(index);
+            password.setCharAt(index, password.charAt(i));
+            password.setCharAt(i, temp);
+        }
+
+        return password.toString();
+    }
+
+    private static char getRandomChar(String charSet, SecureRandom random) {
+        int index = random.nextInt(charSet.length());
+        return charSet.charAt(index);
+    }
+
+    public static void main(String[] args) {
+        int passwordLength = 15;
+        String password = generatePassword(passwordLength);
+        System.out.println("Generated Password: " + password);
+    }
+}
